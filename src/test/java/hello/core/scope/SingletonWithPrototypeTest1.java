5b975311a03fe5e3f21f33b7fb2d6ef4a0404e47
Chae.hyunmin <hmchae@naonsoft.com>
2022-03-21 22:53:03 +0900

    [공부]#empty 스프링핵심원리 의존관계주입_20220321  - 프로토타입 스코프 - 싱글톤 빈과 함께 사용시 provider..  - 웹 스코프  - request스포크 예제 만들기  - 스코프와 Provider  - 스코프로 프록시

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,14 +1,12 @@
 package hello.core.scope;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -35,7 +33,7 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(1);
+        assertThat(count2).isEqualTo(2);
 
     }
 
@@ -44,18 +42,13 @@ public class SingletonWithPrototypeTest1 {
         private final PrototypeBean prototypeBean; //생성시점에 주입
 
         @Autowired
-        private Provider<PrototypeBean> prototypeBeanProvider;
-
-        @Autowired
         public ClientBean(PrototypeBean prototypeBean) {
             this.prototypeBean = prototypeBean;
         }
 
         public int logic(){
-            PrototypeBean object = prototypeBeanProvider.get();  //DL(dependency lookup)
-            object.addCount();
-//            prototypeBean.addCount();
-            return object.getCount();
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
         }
 
     }
